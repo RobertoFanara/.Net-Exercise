429e1d5bbd9eb978302a2512ce6d2904f63705ab
RobertoFanara <fanararoberto7@gmail.com>
2023-11-25 21:33:10 +0100

    esercizio-3 completed

---
+++ Esercizio-3/Esercizio-3/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Inserisci il primo numero");
+
+string? firstNumber = Console.ReadLine();
+
+int numberA = int.Parse(firstNumber);
+
+Console.WriteLine("Adesso inserisci il secondo numero");
+
+string? secondNumber = Console.ReadLine();
+
+int numberB = int.Parse(secondNumber);
+
+Console.WriteLine("Ora dichiara l'operatore (+, -, *, /)");
+
+string? op = Console.ReadLine();
+
+
+int result;
+
+switch (op)
+{
+    case "+":
+        result = numberA + numberB;
+        Console.WriteLine($"Bravissimo, il risultato è: {result}");
+        break;
+
+    case "-":
+        result = numberA - numberB;
+        Console.WriteLine($"Bravissimo, il risultato è: {result}");
+        break;
+
+    case "*":
+        result = numberA * numberB;
+        Console.WriteLine($"Bravissimo, il risultato è: {result}");
+        break;
+
+    case "/":
+        result = numberA / numberB;
+        Console.WriteLine($"Bravissimo, il risultato è: {result}");
+        break;
+}
+
+
+
+

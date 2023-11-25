d5223d8161db963ae98a73dba90e41ae664d11a8
RobertoFanara <fanararoberto7@gmail.com>
2023-11-25 21:48:27 +0100

    esercizio-4 completed

---
+++ Esercizio-4/esercizio-4/Program.cs
@@ -0,0 +1,13 @@
+﻿class ReverseString
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input something!");
+        string? str = Console.ReadLine() ;
+        char[] stringArray = str.ToCharArray();
+        Array.Reverse(stringArray);
+        string reversedStr = new string(stringArray);
+        Console.Write($"Actual String is : {str} \n");
+        Console.Write($"Reversed String is : {reversedStr} ");
+    }
+}

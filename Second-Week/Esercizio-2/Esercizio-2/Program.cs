5ca7db842408fb89057675e367b59e9ee836a43d
RobertoFanara <fanararoberto7@gmail.com>
2023-11-28 17:46:07 +0100

    esercizio-2 completed

---
+++ Second-Week/Esercizio-2/Esercizio-2/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] numbers =
+{
+   5,
+   6,
+   2,
+   4,
+   1,
+};
+
+numbers[3] = 3;
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Object[] newArray =
+{
+    5,
+    "hello",
+    27,
+};
+
+foreach (var ob in newArray)
+{
+    Console.WriteLine(ob);
+}
+Console.WriteLine();
+
+
+Object[] newArray2 = new Object[6];
+
+
+Array.Copy(numbers, newArray2, 3);
+Array.Copy(newArray, 0, newArray2, 3, 3);
+
+foreach (var ob in newArray2)
+{
+    Console.WriteLine(ob);
+}

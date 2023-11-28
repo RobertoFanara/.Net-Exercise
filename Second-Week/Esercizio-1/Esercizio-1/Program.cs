6879d639b37b6bd16dcecd32e7d83719239900d9
RobertoFanara <fanararoberto7@gmail.com>
2023-11-28 17:09:45 +0100

    esercizio-2 completed

---
+++ Second-Week/Esercizio-1/Esercizio-1/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> userInputList = new List<string>();
+
+Console.WriteLine("Enter 10 values!");
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Enter value {i + 1}: ");
+    string? userInput = Console.ReadLine();
+
+    userInputList.Add(userInput);
+}
+
+
+foreach (string input in userInputList)
+{
+    Console.WriteLine(input);
+}

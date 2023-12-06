9cc78fbdb7f8c2dc8720a968c5858f5dd7b08618
RobertoFanara <fanararoberto7@gmail.com>
2023-12-06 18:00:20 +0100

    esercizio-7 completed

---
+++ First-Week/Esercizio-7/Esercizio 7/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_7
+{
+    internal class ClassProva
+    {
+        public int Id { get; set; }
+
+        private string? Name { get; set; }
+
+        protected bool? IsPublic { get; set; }
+
+        public void SetPrivateString(string x)
+        {
+            Name = x;
+        }
+
+        public void SetProtectedBool(bool x)
+        {
+            IsPublic = x;
+        }
+
+        public string GetPrivateString()
+        {
+            if (Name == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return Name;
+        }
+
+        public bool? GetProtectedBool()
+        {
+            return IsPublic;
+        }
+    }
+}
---
+++ First-Week/Esercizio-7/Esercizio 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using Esercizio_7;
+
+ClassProva Class1 = new ClassProva();
+
+Class1.Id = 2;
+
+//Class1.Name = "Test";     <-- Ci darà errore a causa del livello di protezione. (Private)
+
+string? name = Console.ReadLine();
+
+if (name == null)
+{
+    throw new InvalidOperationException();
+}
+
+Class1.SetPrivateString(name);
+
+Console.WriteLine(Class1.GetPrivateString());
+
+Console.WriteLine(Class1.Id);
+
+//Class1.IsPublic = false;     <-- Ci darà errore a causa del livello di protezione. (Protected)
+
+if (Class1.Id == 1)
+{
+    Class1.SetProtectedBool(true);
+}
+else
+{
+    Class1.SetProtectedBool(false);
+}
+
+Class1.GetProtectedBool();

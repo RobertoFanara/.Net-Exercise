81048ab9e0a0711c8337ee45fdaff8b3cb6faab7
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 14:17:29 +0100

    esercizio 5 completed

---
+++ Third-Week/Esercizio-5/Program.cs
@@ -0,0 +1,7 @@
+﻿Random random = new Random();   
+var numbers = Enumerable.Range(0,100).Select(i => random.NextDouble()*100).ToList();
+
+Console.WriteLine($"La somma è {numbers.Sum()}");
+Console.WriteLine($"La media è {numbers.Average()}");
+Console.WriteLine($"I numeri presenti nella lista sono {numbers.Count()}");
+

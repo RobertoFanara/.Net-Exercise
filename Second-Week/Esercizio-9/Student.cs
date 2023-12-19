f1647d6164c63860134e252e0e259ad2c9974186
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 13:54:24 +0100

    esercizio9 completed

---
+++ Second-Week/Esercizio-9/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio18
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        public Student(string name, int grade)
+        {
+            this.Name = name;
+            this.Grade = grade;
+        }
+        public override string ToString()
+        {
+            return $"{Name} , voto : {Grade}";
+        }
+    }
+}

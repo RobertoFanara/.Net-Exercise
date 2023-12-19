2496f25454c9e4ef48df93bd5e955e1a387bf66f
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 14:07:50 +0100

    esercizio 3 completed

---
+++ Third-Week/Esercizio-3/Program.cs
@@ -0,0 +1,17 @@
+﻿Random random = new Random();
+
+var ascNumbers = Enumerable.Range(0, 1000).Select(n => random.Next(0,1001)).OrderBy(n=>n);
+
+var descNumbers = ascNumbers.OrderByDescending(n=>n);
+
+Console.WriteLine("1 to 1000 Order: ");
+foreach (var asc in ascNumbers)
+{
+    Console.WriteLine(asc);
+}
+Console.WriteLine("1000 to 1 Order: ");
+foreach (var desc in descNumbers)
+{
+    Console.WriteLine(desc);
+}
+

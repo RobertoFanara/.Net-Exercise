e9c0e9f0d06c0344c6784f93006a7b41ea77d2a8
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 13:45:36 +0100

    esercizio 5 completed

---
+++ Second-Week/Esercizio-5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.Tracing;
+using System.Threading.Channels;
+Console.WriteLine("Write some words, some of this repeatedly");
+File.WriteAllText("file.txt", Console.ReadLine());
+
+string[] lines = File.ReadAllLines("file.txt");
+Dictionary<string,int> map = new Dictionary<string,int>(StringComparer.OrdinalIgnoreCase);
+
+Console.WriteLine("Word Counter:");
+
+foreach (string line in lines)
+{   
+    string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+   foreach (var word in words)
+   {
+       if (map.ContainsKey(word))
+        {
+            map[word] = map[word] + 1;
+        } 
+       else
+        {
+            map.Add(word, 1 );
+        }
+   }
+    foreach (var pair in map)
+    {
+        Console.WriteLine($"You Wrote '{pair.Key}' {pair.Value} Times");
+    }
+
+}
+

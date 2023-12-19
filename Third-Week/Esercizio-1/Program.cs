3eeac6b5ee6e17aa0e6f8eaf010d407254093b08
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 14:02:02 +0100

    esercizio 1 completed

---
+++ Third-Week/Esercizio-1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Random random = new Random();
+var numbers = Enumerable.Range(0,1000).Select(x => random.Next(0,1001)).ToList();
+
+var oddNumbers = from number in numbers
+                 where number % 2 != 0
+                 orderby number
+                 select number;
+
+Console.WriteLine("Odd number list:");
+foreach (var item in oddNumbers)
+{
+    Console.WriteLine(item);
+}

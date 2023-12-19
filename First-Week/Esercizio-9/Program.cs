96bd18291a4f73b0c9af0ce98ea35b151caf4acb
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 10:25:26 +0100

    esercizio 9 completed

---
+++ First-Week/Esercizio-9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Digit '1' to convert °C in °F or '2' to convert °F in °C ");
+string? choice = Console.ReadLine();
+Console.WriteLine("Insert the temperature");
+string? temp= Console.ReadLine();
+double tempNum = double.Parse(temp);
+if (choice.Equals("1"))
+{
+    double tempFahrenheit = (tempNum * 1.8) + 32;
+
+    Console.WriteLine($"{temp}° C correspond to {tempFahrenheit}° F");
+}
+else if (choice.Equals("2"))
+{
+    double tempCelsius = (tempNum - 32) * 1.8;
+    Console.WriteLine($"{temp}° F correspond to {tempCelsius}° C");
+}

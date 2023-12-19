a5176fd1c70de42ff90848f041071146364f7279
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 10:07:12 +0100

    esercizio 5 completed

---
+++ First-Week/Esercizio-5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+Console.WriteLine($"current Directory {Directory.GetCurrentDirectory()}");
+Console.WriteLine("Insert the path of the file you want to open");
+string filePath = Console.ReadLine();
+try
+{
+    string content = File.ReadAllText(filePath);
+    Console.WriteLine(content);
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("File not found");
+}
+catch (Exception ex)     
+{
+    Console.WriteLine(ex.StackTrace);
+}

1f6ec597fb859020fbc04fef3cf45446ad471c99
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 10:19:01 +0100

    esercizio 8 completed

---
+++ First-Week/Esercizio-8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Insert a word");
+string? word = Console.ReadLine();
+string reversedString = "";
+
+for (int i = word.Length - 1; i >= 0; i--){
+
+    if(word == null)
+    {
+        throw new ArgumentException();
+    }
+
+    reversedString += word[i];
+}
+if ( reversedString.Equals(word))
+{
+    Console.WriteLine($"The word you inserted '{word}' is palindrome");
+}
+else
+{
+    Console.WriteLine($"The word you inserted '{word}' isn't palindrome");
+
+
+}

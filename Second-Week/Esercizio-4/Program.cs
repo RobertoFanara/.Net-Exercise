6503547c50d7460ed1ce2ab039bba6d34d842be9
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 13:40:21 +0100

    esercizio 4 completed

---
+++ Second-Week/Esercizio-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using Esercizio14;
+
+
+LinkedList<TrainStation> list = new LinkedList<TrainStation>();
+void Start()
+{
+    TrainStation Firenze = new TrainStation("Firenze", "Via Firenze 9");
+    TrainStation Milano = new TrainStation("Milano", "Piazza IV Novembre");
+    TrainStation Napoli = new TrainStation("Napoli", "Via Pordenone 2");
+    TrainStation Roma = new TrainStation("Roma", "Corso Cavour 15");
+    
+    list.AddFirst(Milano);
+    list.AddLast(Firenze);
+    list.AddLast(Roma);
+    list.AddLast(Napoli);
+}
+void PrintTrainRoute()
+{
+    for (int i = 0; i < list.Count; i++)
+    {
+
+        if (i == 0)
+        {
+            Console.WriteLine($"Start Station -> {list.ElementAt(i)}");
+
+        }
+        else if (i == list.Count - 1)
+        {
+            Console.WriteLine($"End Station -> {list.ElementAt(i)}");
+
+        }
+        else
+        {
+            Console.WriteLine($"Next Station -> {list.ElementAt(i)}");
+
+        }
+    }
+}
+
+Start();
+PrintTrainRoute();
+Console.WriteLine("\nMilano station is closed, this is the new route:");
+list.RemoveFirst();
+list.AddFirst(new TrainStation("Palermo", "Piazza Matteo Boiardo"));
+PrintTrainRoute();
+Console.WriteLine();
+   
+    
+
+
---
+++ Second-Week/Esercizio-4/TrainStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio14
+{
+    internal class TrainStation
+    {
+        
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public TrainStation(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, {Address}";
+        }
+    }
+    
+    
+}

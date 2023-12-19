1fc100d2919d6ec25eb18aeb10d0c66096f298b3
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 14:46:13 +0100

    esercizio 7 completed

---
+++ Third-Week/Esercizio-7/Esercizio-7/Program.cs
@@ -0,0 +1,6 @@
+﻿List<int> numberList5 = new List<int> { 1, 2, 4, 4, 3, 9, 8, 7, 65 };
+int result1 = numberList5.FirstOrDefault(x => x == 3);
+int result2 = numberList5.FirstOrDefault(x => x > 5);
+int result3 = numberList5.FirstOrDefault(x => x < 5);
+int result4 = numberList5.FirstOrDefault(x => x > 81); // Restituisce 0 perche la condizione non viene soddisfatta visto che non esiste nessun numero maggiore di 81
+Console.WriteLine($"FirstOrDefault:\n{result1}\n{result2}\n{result3}\n{result4}");

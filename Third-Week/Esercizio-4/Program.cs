d14f3d07508849269a6e6f2bc6a18b67827f3b95
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 14:15:42 +0100

    esercizio 4 completed

---
+++ Third-Week/Esercizio-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+
+Random random = new Random();
+
+var numbers = Enumerable.Range(0, 1000).Select(n => random.Next(0, 1001));
+var sortedNumbers = numbers.OrderBy(n => n, new intComparer());
+
+foreach (var number in sortedNumbers) 
+{ 
+    Console.WriteLine(number); 
+}
+
+//This is the custom comparer
+
+public class intComparer : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        int xLength = x.ToString().Length;
+        int yLength = y.ToString().Length;
+
+        return xLength.CompareTo(yLength);
+    }
+}

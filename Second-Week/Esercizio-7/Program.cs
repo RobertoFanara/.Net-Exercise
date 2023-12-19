9ecb05c58b3aac7575218b04b116b53658f8c7eb
RobertoFanara <fanararoberto7@gmail.com>
2023-12-19 13:47:53 +0100

    esercizio 7 completed

---
+++ Second-Week/Esercizio-7/Program.cs
@@ -0,0 +1,17 @@
+﻿var random = new Random();
+int maxNumber = 0;
+int minNumber = int.MaxValue;
+
+List<int> list = new List<int>();
+
+for (int i = 0; i < 1000; i++)
+{
+    list.Add(random.Next(0,1001));
+}
+foreach (var item in list)
+{
+    if (item < minNumber) { minNumber = item; }
+    if (item > maxNumber) { maxNumber = item; }
+}
+
+Console.WriteLine($"Max number is: {max}, Min number is: {min}");
